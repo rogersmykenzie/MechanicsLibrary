c8cdfb64d16082e6eb4108e7c93af7ef38fa13c3
rogersmykenzie <44181353+rogersmykenzie@users.noreply.github.com>
2025-10-13 21:50:03 +0000

    Pending changes exported from your codespace

--- MechanicsLibrary.Core/obj/Debug/net9.0/MechanicsLibrary.Core.AssemblyInfo.cs
+++ MechanicsLibrary.Core/obj/Debug/net9.0/MechanicsLibrary.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MechanicsLibrary.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125e2c43c023d3db485cecc9bf74a6bdfa785fa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bfdfc9c6e419aee49cf402c32266931bc0b1b34")]
 [assembly: System.Reflection.AssemblyProductAttribute("MechanicsLibrary.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MechanicsLibrary.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
