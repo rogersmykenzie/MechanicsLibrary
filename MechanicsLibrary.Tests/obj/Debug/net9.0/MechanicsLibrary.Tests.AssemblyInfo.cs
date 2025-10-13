3bfdfc9c6e419aee49cf402c32266931bc0b1b34
rogersmykenzie <44181353+rogersmykenzie@users.noreply.github.com>
2025-10-13 21:16:53 +0000

    test

--- MechanicsLibrary.Tests/obj/Debug/net9.0/MechanicsLibrary.Tests.AssemblyInfo.cs
+++ MechanicsLibrary.Tests/obj/Debug/net9.0/MechanicsLibrary.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MechanicsLibrary.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a4ec7b94ef2bf65796a97553dda17be5884076")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125e2c43c023d3db485cecc9bf74a6bdfa785fa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MechanicsLibrary.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MechanicsLibrary.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
